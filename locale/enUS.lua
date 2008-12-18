77ce18e3a1f43bd2a047bb1d9c3288dfc27ca510
Xocide <xocide@exero.net>
2008-12-18 05:52:50 +0000

    Added AceLocale and an enUS locale file for the future localization support.

---
+++ locale/enUS.lua
@@ -0,0 +1,40 @@
+--
+--	Proculas
+--	Created by Clorell/Keruni of Argent Dawn [US]
+--	$Id$
+--
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Proculas", "enUS", true)
+
+L["GENERAL_SETTINGS"] = "General Settings";
+L["ENABLE_POSTING"] = "Enable posting procs?";
+L["POST_PROCS"] = "Post Procs";
+L["POST_PROCS_DESC"] = "Check to enable posting procs.";
+L["WHERE_TO_POST"] = "Where should Proculas post procs?";
+L["CHAT_FRAME"] = "Chat Frame";
+L["CHAT_FRAME_DESC"] = "Post procs to the chat frame?";
+L["PARTY_CHAT"] = "Party Chat";
+L["PARTY_CHAT_DESC"] = "Post procs to party chat?";
+L["COMBAT_TEXT"] = "Combat Text";
+L["COMBAT_TEXT_DESC"] = "Post procs to combat text?";
+L["RAID_WARNING"] = "Raid Warning";
+L["RAID_WARNING_DESC"] = "Post procs to raid warning?";
+L["GUILD_CHAT"] = "Guild Chat";
+L["GUILD_CHAT_DESC"] = "Post procs to guild chat?";
+L["RAID_CHAT"] = "Raid Chat";
+L["RAID_CHAT_DESC"] = "Post procs to raid chat?";
+L["CONFIG_PROC_MESSAGE"] = "Configure message when things proc.";
+L["BEFORE"] = "Before";
+L["BEFORE_DESC"] = "Text before the Proc Name.";
+L["AFTER"] = "After";
+L["AFTER_DESC"] = "Text after the Proc Name.";
+L["CONFIG_PROC_SOUND"] = "Configure the sound settings for Proculas.";
+L["ENABLE_SOUND"] = "Enable Sound";
+L["ENABLE_SOUND_DESC"] = "Causes Proculas to play a chosen sound effect when something proc.";
+L["SOUND_TO_PLAY"] = "Sound to play";
+L["ABOUT_CMD"] = "About";
+L["ABOUT_CMD_DESC"] = "About Proculas (/proculas about)";
+L["CONFIGURE_CMD"] = "Configure";
+L["CONFIGURE_CMD_DESC"] = "Open the configuration dialog (/proculas config)";
+L["STICKY_COMBAT_TEXT"] = "Sticky Combat Text";
+L["STICKY_COMBAT_TEXT_DESC"] = "Display Combat Text message as Sticky/Crit?";
