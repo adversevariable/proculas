b1f386ab72f7ed0d949972e75bf43fa807a9ba9d
Xocide <xocide@exero.net>
2013-07-01 12:22:45 +1000

    Split up the core file into separate files.

--- Core.lua
+++ Core.lua
@@ -1,9 +1,10 @@
 --
 -- Proculas
 -- Tracks and gatheres stats on Procs.
--- Created by Korvo of Proudmoore [US]
 --
--- File hash: @file-hash@
+-- Copyright (c) Xocide, who is:
+--  - Korvo on US Proudmoore
+--  - Idunnô, Clorell, Mcstabin on US Hellscream
 --
 
 -------------------------------------------------------
@@ -162,89 +163,6 @@ function Proculas:addProcList(list,procs)
 	end
 end
 
--- Scans an item and checks for procs
-function Proculas:scanItem(slotID)
-	local itemlink = GetInventoryItemLink("player", slotID)
-	if itemlink ~= nil then
-		local found, _, itemstring = string.find(itemlink, "^|c%x+|H(.+)|h%[.+%]")
-		if(found) then
-			local _, itemId, enchantId, jewelId1, jewelId2, jewelId3, jewelId4, suffixId, uniqueId, fromLvl = strsplit(":", itemstring)
-			local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture = GetItemInfo(itemlink)
-
-			-- Enchants and Embroideries
-			if tonumber(enchantId) ~= 0 then
-				local enchID = tonumber(enchantId)
-				if(self.procs.ENCHANTS[enchID]) then
-					--if not self.optpc.tracked[enchID] then
-					local ench = self.procs.ENCHANTS[enchID]
-					if not self.optpc.procs[ench.procId] then
-						local procInfo = self.procs.ENCHANTS[enchID]
-						local name, rank, icon, cost, isFunnel, powerType, castTime, minRange, maxRange = GetSpellInfo(procInfo.spellIds[1])
-						procInfo.icon = icon
-						procInfo.name = name
-						procInfo.rank = rank
-						self:addProc(procInfo)
-					end
-				end
-			end
-
-			-- Items
-			itemId = tonumber(itemId)
-			if self.procs.ITEMS[itemId] then
-				--for _,spell in pairs(self.procs.ITEMS[itemId].spellIds) do
-					if not self.optpc.tracked[spell] then
-						local procInfo = self.procs.ITEMS[itemId];
-						procInfo.name = itemName
-						procInfo.icon = itemTexture
-						procInfo.itemId = itemId
-						--procInfo.spellId = spell
-						self:addProc(procInfo)
-					end
-				--end
-			end
-		end
-	end
-end
-
--- Scans for Procs
-function Proculas:scanForProcs()
-	--self:Print("Scanning for procs");
-
-	-- Find Procs
-	self:scanItem(GetInventorySlotInfo("MainHandSlot"))
-	self:scanItem(GetInventorySlotInfo("SecondaryHandSlot"))
-	self:scanItem(GetInventorySlotInfo("Trinket0Slot"))
-	self:scanItem(GetInventorySlotInfo("Trinket1Slot"))
-	self:scanItem(GetInventorySlotInfo("Finger0Slot"))
-	self:scanItem(GetInventorySlotInfo("Finger1Slot"))
-	self:scanItem(GetInventorySlotInfo("HeadSlot"))
-	self:scanItem(GetInventorySlotInfo("NeckSlot"))
-	self:scanItem(GetInventorySlotInfo("ShoulderSlot"))
-	self:scanItem(GetInventorySlotInfo("BackSlot"))
-	self:scanItem(GetInventorySlotInfo("ChestSlot"))
-	self:scanItem(GetInventorySlotInfo("WristSlot"))
-	self:scanItem(GetInventorySlotInfo("HandsSlot"))
-	self:scanItem(GetInventorySlotInfo("WaistSlot"))
-	self:scanItem(GetInventorySlotInfo("LegsSlot"))
-	self:scanItem(GetInventorySlotInfo("FeetSlot"))
-
-	-- Add Class Procs
-	for index,procs in pairs(self.procs) do
-		if(index == playerClass) then
-			for spellID,procInfo in pairs(procs) do
-				procInfo.spellId = spellID
-				local name, rank, icon = GetSpellInfo(spellID)
-				procInfo.icon = icon
-				procInfo.name = name
-				procInfo.rank = rank
-				procInfo.spellIds = {}
-				table.insert(procInfo.spellIds, spellID)
-				self:addProc(procInfo)
-			end
-		end
-	end
-end
-
 -- Adds a proc to the tracked procs
 function Proculas:addProc(procInfo)
 	if not procInfo.rank then
@@ -288,31 +206,6 @@ function Proculas:addProc(procInfo)
 		end
 		self:Print("Added proc: "..procInfo.name);
 	end
-end
-
-function string.trim(str)
-	-- Function by Colandus
-	return (string.gsub(str, "^%s*(.-)%s*$", "%1"))
-end
-
-function string.explode(sep, str)
-	-- Function by Colandus
-	local pos, t = 1, {}
-	--if #sep == 0 or #str == 0 then return end
-	for s, e in function() return string.find(str, sep, pos) end do
-		table.insert(t, string.trim(string.sub(str, pos, s-1)))
-		pos = e+1
-	end
-	table.insert(t, string.trim(string.sub(str, pos)))
-	return t
-end
-
-local function countarray(array)
-	local count = 0
-	for a,b in pairs(array) do
-		count = count+1
-	end
-	return count
 end
 
 -- Adds the proc from the options panel.
@@ -410,64 +303,6 @@ function Proculas:resetProcStats()
 end
 
 -------------------------------------------------------
--- Proc Cooldown Frame
-
--- Create Frame
-function Proculas:CreateCDFrame()
-	self.procCooldowns = self:NewBarGroup(L["Proc Cooldowns"], nil, self.opt.cooldowns.barWidth, self.opt.cooldowns.barHeight, "ProculasProcCD")
-	self.procCooldowns:SetFont(LSM:Fetch('font', self.opt.cooldowns.barFont), self.opt.cooldowns.barFontSize)
-	self.procCooldowns:SetTexture(LSM:Fetch('statusbar', self.opt.cooldowns.barTexture))
-	self.procCooldowns:SetColorAt(1.00, self.opt.cooldowns.colorStart.r, self.opt.cooldowns.colorStart.g, self.opt.cooldowns.colorStart.b, self.opt.cooldowns.colorStart.a)
-	self.procCooldowns:SetColorAt(0.25, self.opt.cooldowns.colorEnd.r, self.opt.cooldowns.colorEnd.g, self.opt.cooldowns.colorEnd.b, self.opt.cooldowns.colorEnd.a)
-	self.procCooldowns.RegisterCallback(self, "AnchorClicked")
-	self.procCooldowns:SetUserPlaced(true)
-	self.procCooldowns:ReverseGrowth(self.opt.cooldowns.reverseGrowth)
-
-	if(self.opt.cooldowns.show) then
-		self.procCooldowns:Show()
-	else
-		self.procCooldowns:Hide()
-	end
-
-	self:setMovableCooldownsFrame(self.opt.cooldowns.movableFrame)
-end
-
--- Anchor Clicked
-function Proculas:AnchorClicked(...)
-	local button = select(3,...)
-	if button == "RightButton" then
-		self:setMovableCooldownsFrame(false)
-	end
-end
-
--- Set Movable
-function Proculas:setMovableCooldownsFrame(movable)
-	self.opt.cooldowns.movableFrame = movable
-	if movable then
-		self.procCooldowns:ShowAnchor()
-	else
-		self.procCooldowns:HideAnchor()
-	end
-end
-
--- Update Frame
-function Proculas:updateCooldownsFrame()
-	self.procCooldowns:SetFont(LSM:Fetch('font', self.opt.cooldowns.barFont), self.opt.cooldowns.barFontSize)
-	self.procCooldowns:SetWidth(self.opt.cooldowns.barWidth)
-	self.procCooldowns:SetHeight(self.opt.cooldowns.barHeight)
-	self.procCooldowns:SetTexture(LSM:Fetch('statusbar', self.opt.cooldowns.barTexture))
-	self.procCooldowns:SetColorAt(1.00, self.opt.cooldowns.colorStart.r, self.opt.cooldowns.colorStart.g, self.opt.cooldowns.colorStart.b, self.opt.cooldowns.colorStart.a)
-	self.procCooldowns:SetColorAt(0.25, self.opt.cooldowns.colorEnd.r, self.opt.cooldowns.colorEnd.g, self.opt.cooldowns.colorEnd.b, self.opt.cooldowns.colorEnd.a)
-	self.procCooldowns:ReverseGrowth(self.opt.cooldowns.reverseGrowth)
-	self:setMovableCooldownsFrame(self.opt.cooldowns.movableFrame)
-	if(self.opt.cooldowns.show) then
-		self.procCooldowns:Show()
-	else
-		self.procCooldowns:Hide()
-	end
-end
-
--------------------------------------------------------
 -- Event Functions
 
 -- Rescans the players gear when they change an item
@@ -490,220 +325,29 @@ local function checkType(types,type)
 	return false
 end
 
-function Proculas:postProc(spellID)
-	local procInfo = self.optpc.tracked[spellID]
-	--local procData = self.optpc.procs[procInfo.name..procInfo.rank]
-	local procData = self.optpc.procs[procInfo.procId]
-
-	-- Sink
-	local pourBefore = ""
-	if(self.opt.sinkOptions.sink20OutputSink == "Channel") then
-		pourBefore = "[Proculas]: "
-	end
-	if procData.postProc or (self.opt.postProcs and (procData.postProc ~= false or procData.postProc == nil)) then
-		local procMessage
-		if procData.customMessage then
-			procMessage = procData.message
-		else
-			procMessage = self.opt.announce.message
-		end
-		local color = nil
-		if not procData.color then
-			color = self.opt.announce.color
-		else
-			color = procData.color
-		end
-		self:Pour(pourBefore..procMessage:format(procData.name),color.r,color.g,color.b);
-	end
+function string.trim(str)
+	-- Function by Colandus
+	return (string.gsub(str, "^%s*(.-)%s*$", "%1"))
 end
 
-function Proculas:processProc(spellID,isAura)
-	local procInfo = self.optpc.tracked[spellID]
-	--local procData = self.optpc.procs[procInfo.name..procInfo.rank]
-	local procData = self.optpc.procs[procInfo.procId]
-
-	if isAura then
-		self.active[procInfo.procId] = spellID
+function string.explode(sep, str)
+	-- Function by Colandus
+	local pos, t = 1, {}
+	--if #sep == 0 or #str == 0 then return end
+	for s, e in function() return string.find(str, sep, pos) end do
+		table.insert(t, string.trim(string.sub(str, pos, s-1)))
+		pos = e+1
 	end
-
-	self:debug("Processing proc: "..procInfo.name)
-
-	-- Post Proc
-	self:postProc(spellID)
-
-	-- Check Cooldown
-	if procData.updateCD and procData.lastProc > 0 and ((procData.cooldown == 0) or (time() - procData.lastProc < procData.cooldown)) then
-		local proccd = time() - procData.lastProc
-		if(proccd == 0) then
-			procData.zeroCD = true
-		end
-		if(procData.zeroCD) then
-			procData.cooldown = 0
-		else
-			procData.cooldown = time() - procData.lastProc
-		end
-	end
-
-	-- Uptime Calculation
-	if isAura and procData.started == 0 then
-			procData.started = time()
-			self.active[procInfo.procId] = spellID
-	end
-
-	-- Reset cooldown bar
-	if (procData.cooldown or (self.opt.cooldowns.cooldowns and (procData.cooldown ~= false or procData.cooldown == nil))) and procData.cooldown > 0 then
-		local bar = self.procCooldowns:GetBar(procData.name..procData.rank)
-		if not bar then
-			bar = self.procCooldowns:NewTimerBar(procData.name..procData.rank, procData.name, procData.cooldown, procData.cooldown, procData.icon, self.opt.cooldowns.flashTimer)
-		end
-		bar:SetTimer(procData.cooldown, procData.cooldown)
-	end
-
-	-- Sound
-	if procData.soundFile ~= nil then
-		PlaySoundFile(LSM:Fetch("sound", procData.soundFile), "Master")
-	elseif self.opt.sound.soundFile and self.opt.sound.playSound then
-		PlaySoundFile(LSM:Fetch("sound", self.opt.sound.soundFile), "Master")
-	end
-
-	-- Flash Screen
-	if procData.flash or (self.opt.effects.flash and (procData.flash ~= false or procData.flash == nil)) then
-		self:Flash()
-	end
-
-	-- Shake Screen
-	if procData.shake or (self.opt.effects.shake and (procData.shake ~= false or procData.shake == nil)) then
-		--self:Shake()
-	end
-
-	-- Count
-	procData.count = procData.count+1
-	procData.lastProc = time()
+	table.insert(t, string.trim(string.sub(str, pos)))
+	return t
 end
 
--------------------------------------------------------
--- The Proc scanner, scans the combat log for proc spells
--- This sweet little Proc Tracker here is Copyright (c) Xocide
-local trackedTypes = {
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REFRESHED",
-	"SPELL_EXTRA_ATTACKS",
-	"SPELL_PERIODIC_DAMAGE",
-	"SPELL_ENERGIZE",
-	"SPELL_DAMAGE",
-	"SPELL_SUMMON",
-	"SPELL_LEECH",
-	"SPELL_DRAIN",
-	"SPELL_DISPEL",
-	-- wait a second...
-}
-local untrackedTypes = {
-	SPELL_AURA_REMOVED = true,
-	-- this is better
-}
-
-function Proculas:COMBAT_LOG_EVENT_UNFILTERED(event,...)
-	--local msg,type,msg2,name,msg3,msg4,name2 = select(1, ...)
-	--local spellId, spellName, spellSchool = select(9, ...)
-  --    timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, spellId, spellName, spellSchool = select(1, ...)
-	local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool = select(1, ...) --, missType, amountMissed
-	if not hideCaster then
-		hideCaster = nil
+local function countarray(array)
+	local count = 0
+	for a,b in pairs(array) do
+		count = count+1
 	end
-
-	-- Check if the type is SPELL_AURA_APPLIED
-	local isaura = false
-	if(event == "SPELL_AURA_APPLIED") then
-		isaura = true
-	end
-
-	if self.optpc.tracked[spellId] and untrackedTypes[event] == nil then
-		-- Fetch procInfo
-		local procInfo = self.optpc.tracked[spellId]
-		local procData = self.optpc.procs[procInfo.procId]
-
-		self:debug("Tracked proc found: "..procInfo.name)
-		self:debug("Event not in untracked list ("..event..")")
-
-		if sourceName == playerName
-		or (sourceName ~= nil and sourceName == UNKNOWN and destName == playerName)
-		or (sourceName == nil and destName ~= nil and destName == playerName) then
-			self:debug("Event is related to player ("..playerName..")")
-			if (procInfo.onSelfOnly == 0 or procInfo.onSelfOnly == false) then
-				self:debug("Sending tracked proc to processProc(): "..procInfo.name)
-				self:processProc(spellId,isaura)
-			elseif(procInfo.onSelfOnly and destName == playerName) then
-				self:debug("Sending tracked proc to processProc(): "..procInfo.name)
-				self:processProc(spellId,isaura)
-			end
-		end
-	end
-
-	-- Aura Removed/Expired
-	if self.optpc.tracked[spellId] and event == "SPELL_AURA_REMOVED" then
-		local procInfo = self.optpc.tracked[spellId]
-		if sourceName == playerName and self.active[procInfo.procId] then
-			local procData = self.optpc.procs[procInfo.procId]
-			for index,spID in pairs(self.active) do
-				procData.started = 0
-				self.active[procInfo.procId] = nil
-			end
-		end
-	end
-end
-
--------------------------------------------------------
--- The Proc scanner, scans the combat log for proc spells
--- This sweet little Proc Tracker here is Copyright (c) Xocide
-function Proculas:old_COMBAT_LOG_EVENT_UNFILTERED(event,...)
-	local msg,type,msg2,name,msg3,msg4,name2 = select(1, ...)
-	local spellId, spellName, spellSchool = select(9, ...)
-
-	-- Check if the type is SPELL_AURA_APPLIED
-	local isaura = false
-	if(type == "SPELL_AURA_APPLIED") then
-		isaura = true
-	end
-
-	-- Check if its a proc
-	if self.optpc.tracked[spellId] then
-		-- Fetch procInfo
-		local procInfo = self.optpc.tracked[spellId]
-		--local procData = self.optpc.procs[procInfo.name..procInfo.rank]
-		local procData = self.optpc.procs[procInfo.procId]
-
-		self:debug("Tracked proc found: "..procInfo.name)
-
-		-- Check if this is the right combat event for the proc
-		if(checkType(procInfo.types,type)) then
-			self:debug("Tracked proc matched combat log event: "..procInfo.name.." / "..type)
-			-- Check if its from the right player
-			if(name == playerName) then
-				if(procInfo.onSelfOnly and name2 == playerName) then
-					self:debug("Sending tracked proc to processProc(): "..procInfo.name)
-					self:processProc(spellId,isaura)
-				elseif not procInfo.onSelfOnly then
-					self:debug("Sending tracked proc to processProc(): "..procInfo.name)
-					self:processProc(spellId,isaura)
-				end
-			elseif(name == nil and name2 == playerName) then
-				self:debug("Sending tracked proc to processProc(): "..procInfo.name)
-				self:processProc(spellId,isaura)
-			end
-		end
-	end
-
-	-- Aura Removed/Expired
-	if self.optpc.tracked[spellId] and type == "SPELL_AURA_REMOVED" then
-		local procInfo = self.optpc.tracked[spellId]
-		if name == playerName and self.active[procInfo.procId] then
-			local procData = self.optpc.procs[procInfo.procId]
-			for index,spID in pairs(self.active) do
-				procData.started = 0
-				self.active[procInfo.procId] = nil
-			end
-		end
-	end
+	return count
 end
 
 -------------------------------------------------------
